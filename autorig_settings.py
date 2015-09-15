2522142f2c40488562f700debf7f873bd49d754c
RyugasakiHu <frostnova53@gmail.com>
2015-09-15 11:53:14 +0800

    Update and rename .gitignore to autorig_settings.py

---
+++ autorig_settings.py
@@ -0,0 +1,45 @@
+rootPath = 'C:/eclipse/test/OOP/AutoRig'
+modulesPath = rootPath + 'Modules/'
+uiPath = rootPath + 'Ui/'
+utilsPath = rootPath + 'Utils/'
+customPath = utilsPath + 'blabla/'
+
+#basename = []
+sides = ['r','l','c']
+suffix = ['jj',# joint joint
+          'jc',# joint control          
+          'je',# joint end
+          'ik',# ik joint
+          'ikRP',# ik Rotate Plane
+          'ikSC',# ik Single Chain
+          'ikNF',# ik no flip
+          'fk',# fk joint
+          'grp',# group
+          'loc',# locator
+          'cc',# control curve
+          'loc',# locator
+          'dist',# distance
+          'pv',# pole vector
+          'iks',# ikHandle
+          'gud',# guide
+          'off',# offset
+          'cog',# cog
+          'GUD',# guide grp
+          'BCN',# blendColorNode
+          'COND',#conditionNode
+          'REV',# reverseNode
+          'MDN',# multipleDivideNode
+          'PMA',# plus minus averageNode
+          'RV',#remap valueNode
+          'CONFIG',# WTF
+          'ALL',#ALL grp
+          'TRS',#TRS grp
+          'PP',#PP grp
+          'SKL',#SKL grp
+          'CC',#CC grp
+          'LOC',#LOC grp
+          'IK',#IK grp
+          'GEO',#GEO grp
+          'XTR',#XTR grp
+          'CSG',#correct shape grp
+          ]
