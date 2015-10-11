42fd29cfc3b9189c9878c6f25fad819672024a58
RyugasakiHu <frostnova53@gmail.com>
2015-10-11 23:00:45 +0800

    Update fingerModule.py

--- Moudles/fingerModule.py
+++ Moudles/fingerModule.py
@@ -3,7 +3,6 @@ from subModules import fkChain,ikChain,boneChain,ribbon
 from Utils import nameUtils
 from Modules import control,hierarchy,limbModule
 from see import see
-from __builtin__ import chr
 
 class FingerModule(object):
     
@@ -30,7 +29,16 @@ class FingerModule(object):
         
         #cc
         self.config_node = None
-        self.indexSDK = []
+        self.indexSdks = []
+        
+        #attrs
+#         self.attrs = ['fist_a','firs_b','relax_a','relax_b','relax_c',
+#                       'relax_d','relax_e','grab_a','grab_b','spread_a',
+#                       'spread_b','hand','plam_a','plam_b','thumb_a',
+#                       'thumb_b','index_curl','middle_curl','ring_curl',
+#                       'pinky_curl','point']
+        
+        self.attrs = ['fist_a']
             
         #nameList
         self.fingerName = ['thumb','index','middle','ring','pinky']
@@ -146,15 +154,12 @@ class FingerModule(object):
         pm.setAttr(self.lm.config_node.control + '.___',e = 1,channelBox = 1)
         
         #add attr
-        control.addFloatAttr(self.lm.config_node.control,['fist_a','firs_b','relax_a','relax_b','relax_c',
-                                                          'relax_d','relax_e','grab_a','grab_b','spread_a',
-                                                          'spread_b','hand','plam_a','plam_b','thumb_a',
-                                                          'thumb_b','index_curl','middle_curl','ring_curl',
-                                                          'pinky_curl','point'],-3,10)
+        control.addFloatAttr(self.lm.config_node.control,self.attrs,-3,10)
 
     def __fingerCC(self):
         
-        self.indexSDK = []
+        #create index cc ctrl
+        self.indexSdks = []
         
         for num,indexJoint in enumerate(self.indexBlendChain.chain):
             if num < self.indexBlendChain.chainLength() - 1:
@@ -166,7 +171,7 @@ class FingerModule(object):
                 cc.circleCtrl()
                 pm.rename(cc.control,nameUtils.getUniqueName(self.side,self.fingerName[1] + self.fingerJoint[num],'cc'))
                 pm.rename(cc.controlGrp,nameUtils.getUniqueName(self.side,self.fingerName[1] + self.fingerJoint[num],'SDK'))
-                self.indexSDK.append(cc.controlGrp)
+                self.indexSdks.append(cc.controlGrp)
                 
                 #align cc grp
                 pm.xform(cc.controlGrp,ws = 1,matrix = indexJoint.worldMatrix.get())
@@ -176,35 +181,90 @@ class FingerModule(object):
                 self.indexBlendChain.chain[num + 1].setParent(cc.control)   
            
     def __setSDK(self):
-        
-        #set val
-        indexRotList = [[0.0,1.167,0.0],[7.726,98.472,-6.923],[-5.086,109.129,-14.339],[0.0,121.353,0.0]]
-        rotateAttrs = []
-        rotateVals = [] 
+
+        #prepare chr:
         chr = ['x','y','z']
+        
+        #set val:
+        indexMainMaxRotList = []
+        indexMainMinRotList = []
+        
+        #fist max(10) val:
+        indexFistaMaxRotList = [[0.0, 1.1667451, 0.0],[7.7256914, 98.47196, -6.9225698],[-5.0864835, 109.12878, -14.33877],[0.0, 121.35306, 0.0]]
+        midFistaMaxRotList = [[0.0, 0.0, 0.0],[1.343452, 94.24476, -0.17447495],[-1.2531719, 111.28286, -3.2134328],[0.0, 111.31801, 0.0]]
+        ringFistaMaxRotList = [[-2.2915976, 0.99224884, 0.3515861],[-1.654204, 94.229347, 8.2182298],[1.4022289, 107.094, 4.5496722],[0.0, 118.21848, 0.0]]
+        pinkyFistaMaxRotList = [[-3.4711895, 4.0933869, 1.0393476],[-3.908681, 104.27619, 16.853265],[3.2222396, 102.18746, 14.587789],[0.0, 96.374496, 0.0]]
+        
+        indexMainMaxRotList.append(indexFistaMaxRotList)
                 
-        #set defaut sdk
-        for SDK in self.indexSDK:
-            for vec in ['t','r']:
-                for ch in chr:            
-                    pm.setDrivenKeyframe(SDK + '.' + vec + ch, cd = self.lm.config_node.control + '.fist_a' )
+        #fist min(-3) val:        
+        indexFistaMinRotList = [[0.0, 0.0, 0.0], [2.3656792422, -23.6599771482, 1.9016638177], [1.52594527386, -22.6379875224, 4.30163088994], [0.0, -26.3052739235, 0.0]]
+        midFistaMinRotList = [[0.0, 0.0, 0.0], [0.0, -13.0502537762, 0.0], [0.0, -23.0761814451, 0.964029917432], [0.0, -23.0867256252, 0.0]]
+        ringFistaMinRotList = [[0.0, 0.0, 0.0], [-8.0276, -10.3753295339, -0.732478856346], [0.0, -17.8428903421, -1.36490170652], [0.0, -21.1802374187, 0.0]]
+        pinkyFistaMinRotList = [[1.04135682811, -1.22801612104, 0.0], [-8.96452963728, -10.9554773142, -2.95571135541], [-0.966671923269, -14.4907648544, -4.37633648762], [0.0, -12.7468772018, 0.0]]
         
-        #get attr data
-        for SDK in self.indexSDK:
-            for ch in chr:
-                attr = SDK + '.r' + ch
-                rotateAttrs.append(attr)
-        
-        #get rotate val
-        for index in indexRotList:
-            for num in [0,1,2]:
-                val = index[num]
-                rotateVals.append(val)
+        indexMainMinRotList.append(indexFistaMinRotList)
         
         #set dic
-        for num,attr in enumerate(rotateAttrs):
-            pm.setDrivenKeyframe(attr,v = rotateVals[num],
-                                cd = self.lm.config_node.control + '.fist_a',dv = 10 )
+        indexRotAttrs = []
+        indexFistMaxRotVals = [] 
+        indexFistMinRotVals = []
+        
+        indexMainMaxRotVals = [indexFistMaxRotVals]
+        indexMainMinRotVals= [indexFistMinRotVals]
+        
+        
+#         midRotAttrs = []
+#         midFistMaxRotVals = [] 
+#         midFistMinRotVals = []
+#         
+#         ringRotAttrs = []
+#         ringFistMaxRotVals = [] 
+#         ringFistMinRotVals = []
+#         
+#         pinkyRotAttrs = []
+#         pinkyFistMaxRotVals = [] 
+#         pinkyFistMinRotVals = []
+
+        #get attr data
+        for indexSdk in self.indexSdks:
+            for ch in chr:
+                attr = indexSdk + '.r' + ch
+                indexRotAttrs.append(attr)
+                                
+        #set defaut(0) sdk
+        for indexSdk in self.indexSdks:
+            for ch in chr:            
+                pm.setDrivenKeyframe(indexSdk + '.r' + ch,v = 0, 
+                                     cd = self.lm.config_node.control + '.fist_a',dv = 0)
+#             for vec in ['t','r']:                
+#                 pm.setDrivenKeyframe(indexSdk + '.' + vec + ch,v = 0, 
+#                                      cd = self.lm.config_node.control + '.fist_a',dv = 0)            
+    
+        for selfAttr in self.attrs:
+            #get rotate Max val
+            for valueList in indexMainMaxRotList:
+                for value in valueList:
+                    for num in [0,1,2]:
+                        val = value[num]
+                        indexFistMaxRotVals.append(val)
+            
+            #set dic max(10) val
+            for indexMaxRotVals in indexMainMaxRotVals:
+                for num,attr in enumerate(indexRotAttrs):
+                    pm.setDrivenKeyframe(attr,v = indexMaxRotVals[num],
+                                        cd = self.lm.config_node.control + '.' + selfAttr,dv = 10)
+     
+            #get rotate Min val
+            for index in indexFistaMinRotList:
+                for num in [0,1,2]:
+                    val = index[num]
+                    indexFistMinRotVals.append(val)
+     
+            #set dic min(-3) val
+            for num,attr in enumerate(indexRotAttrs):
+                pm.setDrivenKeyframe(attr,v = indexFistMinRotVals[num],
+                                    cd = self.lm.config_node.control + '.' + selfAttr,dv = -3)
  
         def __cleanUp():
             pass                        
