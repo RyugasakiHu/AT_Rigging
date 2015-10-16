7156bace0e0aa1d59f8f0a0987642845858f9e64
RyugasakiHu <frostnova53@gmail.com>
2015-10-16 09:46:32 +0800

    Update reloadModules.py

--- Moudles/reloadModules.py
+++ Moudles/reloadModules.py
@@ -1,11 +1,13 @@
-import control,limbModule,hierarchy,legModule,footModule,fingerModule
+import control,limbModule,hierarchy,legModule,footModule,fingerModule,spineModule
 
 def reloadIt():
+    
     reload(control)
     reload(limbModule)
     reload(hierarchy)
     reload(legModule)
     reload(footModule)
     reload(fingerModule)
+    reload(spineModule)
     
     print '----->Modules Reload : OK'
