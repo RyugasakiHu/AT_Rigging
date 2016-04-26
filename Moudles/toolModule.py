a0340f533c89fc7394845fea671f5f05777269f6
RyugasakiHu <frostnova53@gmail.com>
2016-04-26 16:27:27 +0800

    Update toolModule.py
    update:
    1.change function to class for twist

--- Moudles/toolModule.py
+++ Moudles/toolModule.py
@@ -8,7 +8,7 @@ import maya.mel as mel
 import maya.cmds as mc
 from Modules.subModules import fkChain,ikChain,boneChain
 from Utils import nameUtils,metaUtils
-from Modules import control,hierarchy,legModule
+from Modules import control,hierarchy,legModule 
 from maya import OpenMaya
  
 
@@ -50,8 +50,7 @@ class ToolModuleUi(object):
         #splitJoint
         self.splitJoint = pm.frameLayout('split Joint Tool : ',cll=1,p = self.mainL)
         self.splitNum = pm.intSliderGrp('intSliderGrp2',min = 1,max = 10,f = 1,s = 1,v = 1)
-        pm.button('split Joint',c = self.__splitJoint)
-        pm.button('create Box',c = self.__createJointBox)
+        pm.button('split Joint',c = self.__splitJointButton) 
         
         #shapeSize
         self.shape = pm.frameLayout('curve Shape Tool : ',cll=1,p = self.mainL)  
@@ -93,123 +92,13 @@ class ToolModuleUi(object):
         print 'Skin joint from ' + metaT + ' are ' + str(jointStr.split('.')[0])
         print 'Number of ' + metaT + ' is ' + str(len(jointStr))    
             
-    def __splitJoint(self,*args):
+    def __splitJointButton(self,*args):
         
+        sel = pm.ls(sl = 1)
+        splitJoint = sel[0] 
         num = pm.intSliderGrp(self.splitNum,q = 1,v = 1)
-        parent = pm.ls(sl = 1)
-        child = pm.listRelatives(parent,c = 1,typ = 'joint')
-        parentPos = pm.xform(parent,q = 1,ws = 1,t = 1)
-        childPos = pm.xform(child,q = 1,ws = 1,t = 1)
-        insertJoints = []
-        
-        #unparent
-        pm.parent(child,w = 1)
-        
-        for i in range(0,num):
-            
-            pm.select(parent,r = 1) 
-            tempPos = [(parentPos[0] - childPos[0])*(i + 1)/(num + 1) + childPos[0],
-                       (parentPos[1] - childPos[1])*(i + 1)/(num + 1) + childPos[1],
-                       (parentPos[2] - childPos[2])*(i + 1)/(num + 1) + childPos[2]]
-            radius = pm.getAttr(parent[0] + '.radius')
-            insertJoint = pm.joint(rad = radius,p = (tempPos[0],tempPos[1],tempPos[2]),n = parent[0] + "_insert_" + str(num - i)) 
-            insertJoints.append(insertJoint)
-            
-            if i == 0:
-                pm.parent(child[0],parent[0] + "_insert_" + str(num - i))
-            else :    
-                pm.parent(parent[0] + "_insert_" + str(num - i + 1),parent[0] + "_insert_" + str(num - i))
-                
-        #select for next function       
-        pm.select(cl = 1) 
-        pm.select(parent) 
-        for insertJoint in insertJoints:
-            pm.select(insertJoint,add = 1)
-        pm.select(child,add = 1)        
-            
-    def __createJointBox(self,*args):
-        
-        a = 1
-        count = 0
-        joints = pm.ls(sl=1,type='joint')       
-        child = pm.listRelatives(c=1) 
-        #shader ckeck:
-        if not pm.objExists('green'):
-            green = pm.shadingNode('lambert', asShader=True,n='green')
-            pm.setAttr(green + '.color',0,1,0,type = "double3")
-            pm.sets(r=True,nss=True,em=True,n='greenSG') 
-            pm.connectAttr('green.outColor','greenSG.surfaceShader',f=True)
-            
-            red = pm.shadingNode('lambert', asShader=True,n='red')
-            pm.setAttr(red + '.color',1,0,0,type = "double3") 
-            pm.sets(r=True,nss=True,em=True,n='redSG') 
-            pm.connectAttr('red.outColor','redSG.surfaceShader',f=True)
-            
-            blue = pm.shadingNode('lambert', asShader=True,n='blue')
-            pm.setAttr(blue + '.color',0,0,1,type = "double3") 
-            pm.sets(r=True,nss=True,em=True,n='blueSG') 
-            pm.connectAttr('blue.outColor','blueSG.surfaceShader',f=True)
-            
-        #ready to roll:
-        for joint in joints:        
-            cube = pm.polyCube(n=str(joint) + '_geo0' + str(a),sx=1,sy=1,sz=1)
-            pm.parent(cube,joint)
-            if count < (len(joints)-1):
-                tx = pm.getAttr(child[count] + '.tx')
-                ty = pm.getAttr(child[count] + '.ty')
-                tz = pm.getAttr(child[count] + '.tz')
-                        
-                abtx = abs(tx)
-                abty = abs(ty)
-                abtz = abs(tz)
-                
-                pm.setAttr(cube[0] + '.tx',tx * 0.5)
-                pm.setAttr(cube[0] + '.ty',ty * 0.5)
-                pm.setAttr(cube[0] + '.tz',tz * 0.5)
-                pm.setAttr(cube[0] + '.r',0,0,0)
-            
-                radius = pm.getAttr(joint + '.radius')
-            
-                if abtx > 0.001:
-                    sx = abtx
-                else :    
-                    sx = radius
-                
-                if abty > 0.001:
-                    sy = abty
-                else :    
-                    sy = radius
-           
-                if abtz > 0.001:
-                    sz = abtz
-                else :    
-                    sz = radius            
-            
-                pm.setAttr(cube[0] + '.sx',sx)
-                pm.setAttr(cube[0] + '.sy',sy)
-                pm.setAttr(cube[0] + '.sz',sz)
-                
-                #set material
-                greenFace = pm.select(cube[0] + '.f[0]',cube[0] + '.f[2]')  
-                mc.sets(fe = 'greenSG',e=True)
-            
-                
-                redFace = pm.select(cube[0] + '.f[1]',cube[0] + '.f[3]') 
-                mc.sets(fe = 'redSG',e=True)
-                
-                blueFace = pm.select(cube[0] + '.f[4]',cube[0] + '.f[5]') 
-                mc.sets(fe = 'blueSG',e=True)
-    
-                #clear history
-                pm.select(cube[0])
-                pm.makeIdentity(cube[0],t=1,s=1,r=1,a=1)
-                
-                #go head TAC-com
-                a += 1
-                count += 1
-                
-        #break the cycle delete the tail       
-        pm.delete(str(joint) + '_geo0' + str(a))    
+        splitJoint =  SplitJoint(splitJoint,num,box = 0,type = 'tool')   
+        splitJoint.splitJointTool()
         
     def __scaleCv(self,*args):
         
@@ -289,8 +178,154 @@ class ToolModuleUi(object):
                 pm.xform(tarCv,ws = 1,t = tempPosList[num])
                 
             pm.select(cl = 1)    
+
+
+class SplitJoint(object):
+    
+    def __init__(self,splitJoint,num,type,box):
+        
+        self.splitJoint = splitJoint
+        self.num = num 
+        self.box = box
+        self.joints = None 
+        self.cubeList = None
+        self.type = type
+                     
+    def splitJointTool(self): 
+        
+        self.joints = []
+        insertJointList = []
+        
+        pm.select(self.splitJoint) 
+        parent = pm.ls(sl = 1)
+        child = pm.listRelatives(parent,c = 1,typ = 'joint')
+        
+        parentPos = pm.xform(parent,q = 1,ws = 1,t = 1)
+        childPos = pm.xform(child,q = 1,ws = 1,t = 1)
+         
+        self.joints.append(parent[0]) 
+         
+        #unparent
+        pm.parent(child,w = 1)
+         
+        for i in range(0,self.num):
+             
+            pm.select(parent,r = 1) 
+            tempPos = [(parentPos[0] - childPos[0])*(i + 1)/(self.num + 1) + childPos[0],
+                       (parentPos[1] - childPos[1])*(i + 1)/(self.num + 1) + childPos[1],
+                       (parentPos[2] - childPos[2])*(i + 1)/(self.num + 1) + childPos[2]]
+            radius = pm.getAttr(parent[0] + '.radius')
+            insertJoint = pm.joint(rad = radius,p = (tempPos[0],tempPos[1],tempPos[2]),
+                                   n = (parent[0].name() + '_insert'))  
+            insertJointList.append(insertJoint) 
+        
+        for num,joint in enumerate(insertJointList):
+            if num != len(insertJointList) - 1:
+                pm.parent(insertJointList[num],insertJointList[num + 1])
+        
+        insertJointList.reverse()
+        for insertJoint in insertJointList:
+            self.joints.append(insertJoint)        
+         
+        pm.parent(insertJointList[0],parent)
+        pm.parent(child,insertJointList[-1])
+        
+        self.joints.append(child[0])
+        print self.joints    
+        
+        if self.box == 1:
+            self.boxCreate()
+        
+    def boxCreate(self):     
+        
+        self.cubeList = []
+        #bos create
+        #local variable
+        a = 1
+        count = 0
+        pm.select(cl = 1)
+        
+        for joint in self.joints:
+            pm.select(joint,add = 1) 
+         
+        joints = pm.ls(sl=1,type='joint')       
+        child = pm.listRelatives(c=1) 
+         
+        #shader check:
+        if not pm.objExists('green'):
+            green = pm.shadingNode('lambert', asShader=True,n='green')
+            pm.setAttr(green + '.color',0,1,0,type = "double3")
+            pm.sets(r=True,nss=True,em=True,n='greenSG') 
+            pm.connectAttr('green.outColor','greenSG.surfaceShader',f=True)
+             
+            red = pm.shadingNode('lambert', asShader=True,n='red')
+            pm.setAttr(red + '.color',1,0,0,type = "double3") 
+            pm.sets(r=True,nss=True,em=True,n='redSG') 
+            pm.connectAttr('red.outColor','redSG.surfaceShader',f=True)
+             
+            blue = pm.shadingNode('lambert', asShader=True,n='blue')
+            pm.setAttr(blue + '.color',0,0,1,type = "double3") 
+            pm.sets(r=True,nss=True,em=True,n='blueSG') 
+            pm.connectAttr('blue.outColor','blueSG.surfaceShader',f=True)
+             
+        #rolling:
+        for joint in joints:       
+            cube = pm.polyCube(n=joint.name() + '_geo_' + str(a),sx=1,sy=1,sz=1)
+            self.cubeList.append(cube)
+            pm.parent(cube,joint)
+            if count < (len(joints)-1):
+                tx = pm.getAttr(child[count] + '.tx')
+                ty = pm.getAttr(child[count] + '.ty')
+                tz = pm.getAttr(child[count] + '.tz')
+                         
+                abtx = abs(tx)
+                abty = abs(ty)
+                abtz = abs(tz)
                  
-    
-    
-    
+                pm.setAttr(cube[0] + '.tx',tx * 0.5)
+                pm.setAttr(cube[0] + '.ty',ty * 0.5)
+                pm.setAttr(cube[0] + '.tz',tz * 0.5)
+                pm.setAttr(cube[0] + '.r',0,0,0)
+             
+                radius = pm.getAttr(joint + '.radius')
+             
+                if abtx > 0.001:
+                    sx = abtx
+                else :    
+                    sx = radius
+                 
+                if abty > 0.001:
+                    sy = abty
+                else :    
+                    sy = radius
+            
+                if abtz > 0.001:
+                    sz = abtz
+                else :    
+                    sz = radius            
+             
+                pm.setAttr(cube[0] + '.sx',sx)
+                pm.setAttr(cube[0] + '.sy',sy)
+                pm.setAttr(cube[0] + '.sz',sz)
+                 
+                #set material
+                greenFace = pm.select(cube[0] + '.f[0]',cube[0] + '.f[2]')  
+                mc.sets(fe = 'greenSG',e=True) 
+                 
+                redFace = pm.select(cube[0] + '.f[1]',cube[0] + '.f[3]') 
+                mc.sets(fe = 'redSG',e=True)
+                 
+                blueFace = pm.select(cube[0] + '.f[4]',cube[0] + '.f[5]') 
+                mc.sets(fe = 'blueSG',e=True)
+     
+                #clear history
+                pm.select(cube[0])
+                pm.makeIdentity(cube[0],t=1,s=1,r=1,a=1)
+                 
+                #go head TAC-com
+                a += 1
+                count += 1
+                
+        #break the cycle delete the tail       
+        pm.delete(self.cubeList[-1])    
                 
